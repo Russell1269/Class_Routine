d4391c2d4ab25d96c572087039c884dd08a55dbe
Rasel Hasan <raselhasanshovo@gmail.com>
2024-10-24 20:02:02 +0600

    new js file added

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-let n=5;
-for(let i=0;i<n;i++){
-    console.log(i," Hello");
-}
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log(i," Hello");
+// }
+
+// let args = process.argv;
+// for (let i = 2; i < args.length; i++) {
+//   console.log("Hello and Welcome to ", args[i]);
+// }
+const math=require("./math");
+
+console.log(math);
---
+++ math.js
@@ -0,0 +1,11 @@
+module.exports.sum = (a, b) => a + b;
+exports.mul = (a, b) => a * b;
+const PI = 3.1416;
+const g = 9.8;
+
+// module.exports=sum();
+let obj = {
+  pi: PI,
+  g: g,
+};
+module.exports = obj;

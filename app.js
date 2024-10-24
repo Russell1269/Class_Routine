3c79cbbdc78c564f340d1da0fa9192b36ca95141
Rasel Hasan <raselhasanshovo@gmail.com>
2024-10-24 20:34:43 +0600

    adding NPM

--- app.js
+++ app.js
@@ -7,6 +7,22 @@
 // for (let i = 2; i < args.length; i++) {
 //   console.log("Hello and Welcome to ", args[i]);
 // }
-const math=require("./math");
+// const math = require("./math");
 
-console.log(math);
+// console.log(math);
+const pt=require('prompt-sync');
+
+const prompt=pt();
+
+//Logic for Fibonacci number
+
+// let str = prompt("Enter a number");
+// let a =parseInt(str);
+// let t1 = 0;
+// let t2 = 1;
+// for (let i = 0; i < a; i++) {
+//   console.log(t1);
+//   let sum = t1 + t2;
+//   t1 = t2;
+//   t2 = sum;
+// }
